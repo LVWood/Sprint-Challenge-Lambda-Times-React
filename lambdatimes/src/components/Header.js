27480a083018af7ca1c0d9e71e20b5fca2d7afa7
LVWood <leslee.wood@gmail.com>
2019-05-10 12:20:03 -0500

    refactored header with styled component

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { HeaderComponent, DateComponent, TempComponent, TitleComponent } from './StyleComponents'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+// const Header = () => {
+//   return (
+//     <div className="header">
+//       <span className="date">SMARCH 32, 2018</span>
+//       <h1>Lambda Times</h1>
+//       <span className="temp">98°</span>
+//     </div>
+//   )
+// }
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderComponent>
+      <DateComponent>SMARCH 32, 2018</DateComponent>
+      <TitleComponent>Lambda Times</TitleComponent>
+      <DateComponent>98°</DateComponent>
+    </HeaderComponent>
   )
 }
 
--- lambdatimes/src/components/StyleComponents.js
+++ lambdatimes/src/components/StyleComponents.js
@@ -76,4 +76,62 @@ const RightSpan = styled.div`
     cursor: pointer;
     `;
 
-export { TopBarComponent, TopContainerComponent, TopContainerLeft, TopContainerCenter, TopContainerRight, LeftSpan, RightSpan, CenterSpan }
+const HeaderComponent = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    background-color: #fff;
+    border-bottom: 1px solid lightgrey;
+    width: 100%;
+    height: 120px;
+    margin-top: 44px;
+    padding-bottom: 15px;
+    }
+    @media (min-width: 1280px) {
+    .header {
+    width: 1280px;
+    }
+    `;
+
+const DateComponent = styled.div`
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    margin-left: 25px;
+    flex: 1;
+    `;
+
+const TempComponent = styled.div`
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    text-align: right;
+    margin-right: 25px;
+    flex: 1;
+    `;
+
+const TitleComponent = styled.h1`
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+    `;
+
+export { 
+    TopBarComponent, 
+    TopContainerComponent, 
+    TopContainerLeft, 
+    TopContainerCenter, 
+    TopContainerRight, 
+    LeftSpan, 
+    RightSpan, 
+    CenterSpan, 
+    HeaderComponent, 
+    DateComponent, 
+    TempComponent,
+    TitleComponent }
